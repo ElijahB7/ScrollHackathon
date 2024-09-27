19616e17963abcc5bb4c4cc6a370f55e1b9d3c52
Elijah Bryan <91127021+ElijahB7@users.noreply.github.com>
2024-09-26 21:03:55 -0400

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -6,14 +6,14 @@ module.exports = {
   solidity: "0.8.26",
   networks: {
     sepolia: {
-      url: "https://scroll-sepolia.g.alchemy.com/v2/43mKUgcLH6NSL6qe1lskxFgmwQxs9N2u", // URL for the Sepolia Scroll node
-      accounts: ["b2ac8c38d57072a307707df27360aba96c5496cd06cc3a72487590c4f3bb5812"], // Your wallet's private key
+      url: "https://scroll-sepolia.g.alchemy.com/v2/ALCHEMYAPIKEY", // URL for the Sepolia Scroll node
+      accounts: ["ADD PRIVATE KEY"], // Your wallet's private key
       chainId: 534351 // Make sure this is the correct chain ID for Sepolia Scroll
     }
   },
   etherscan: {
     apiKey: {
-      scrollSepolia: '7K9PHY7VXHWHYART7DRRGHH37U2FPUQ6MA',
+      scrollSepolia: 'ADD SEPOLIA SCAN KEY',
     },
     customChains: [
       {
